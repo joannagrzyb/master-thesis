54b371d60cf009f73a8544d0aaa4cdb6186185d7
joannagrzyb <joanna.grzyb96@gmail.com>
2019-12-04 21:16:56 +0100

    Basic example

---
+++ basic_example.py
@@ -0,0 +1,17 @@
+import strlearn as sl
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
+stream = sl.streams.StreamGenerator()
+clfs = [
+    GaussianNB(),
+    MLPClassifier()
+]
+
+evaluator = sl.evaluators.TestThenTrainEvaluator()
+evaluator.process(stream, clfs)
+
+print(evaluator.scores_, evaluator.scores_.shape)
+
+
+# napisane przez Ksieniewicza, dziala
